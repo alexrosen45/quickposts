a46bc5fa871d8505703887e497eaabf46513aac7
Justus Croskery <justus.croskery@gmail.com>
2023-01-21 05:05:20 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Installs the required packages and sets up the machine environment
-git clone https://github.com/alexrosen45/quickposts
-cd quickposts
+# git clone https://github.com/alexrosen45/quickposts
+# cd quickposts
+# ./install.sh
 apt-get install python3 python3-pip
 pip install -r requirements.txt
 
