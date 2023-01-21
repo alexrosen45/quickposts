fec36c7ddd2042cc42dd20c4ae86555e00ab5d01
alex <alexanderrosen45@gmail.com>
2023-01-21 18:43:03 -0500

    signout and url restrictions

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="/signin")
 def home(request):
-    if request.method == "POST":
-        print(request.POST)
-    return render(request, 'index.html', context={'data': [{"prompt": "Test", "datetime":"friday", "details": "Testinfo", "status": "Pending"}]})
+    return render(request, 'index.html', context={
+        'data': [{
+            "prompt": "Test",
+            "datetime": "friday",
+            "details": "Testinfo",
+            "status": "Pending"
+        }]
+    })
