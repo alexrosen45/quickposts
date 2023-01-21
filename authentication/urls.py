c577f0e73c81234b5da420e2512650bbc9ef8a84
alex <alexanderrosen45@gmail.com>
2023-01-21 04:01:51 -0500

    authentication app

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
+    path('/signup', views.signup, name='signup')
 ]
--- quickposts/settings.py
+++ quickposts/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # django apps
+    'authentication',
     'home',
     'posts',
 ]
