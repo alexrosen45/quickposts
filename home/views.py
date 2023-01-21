6aae7cbbaff30ec6bc350fe1ae0a81365914a3ce
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-01-21 14:29:34 -0500

    dashboard changes

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def home(request):
     if request.method == "POST":
         print(request.POST)
-    return render(request, 'index.html', context={'data': [{}]})
+    return render(request, 'index.html', context={'data': [{"prompt": "Test", "datetime":"friday", "details": "Testinfo", "status": "Pending"}]})
