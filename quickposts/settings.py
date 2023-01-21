9e7c47757b1c34d3ae7e498671523994618ad6e8
Justus Croskery <justus.croskery@gmail.com>
2023-01-21 19:27:01 +0000

    Rename logo.png and change python settings

--- quickposts/settings.py
+++ quickposts/settings.py
@@ -142,7 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = '/var/www/html/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    "static/",
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
