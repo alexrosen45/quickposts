67afb8ea22f0bfc20322f629c06bf60ae69cca6e
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-01-21 19:11:20 -0500

    accordion stuff

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def home(request):
     if request.method == "POST":
         print(request.POST)
-    return render(request, 'index.html', context={'data': [{"prompt": "Test", "datetime":"friday", "details": "Testinfo", "status": "Pending"}]})
+    return render(request, 'index.html', context={'data': [{"prompt": "Test", "datetime":"friday", "details": "Testinfo", "status": "Pending"}, {"prompt": "Test", "datetime":"friday", "details": "Testinfo", "status": "Pending"}]})
